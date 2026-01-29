bc5768253372c30dedad4a1c684bb54593274de7
Amit Nayak <your-email@example.com>
2026-01-29 09:17:07 +0530

    Fix bcryptjs import typo

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypts";
+import bcrypt from "bcryptjs";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
